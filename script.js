388aa32d2d85a72188650dd44da09c3a764a0877
Jelena Jovicic <jejovicic@gmail.com>
2022-01-15 08:14:56 +0100

    Simplify cards, start on counter

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("howdy")
+console.log("howdy");
+
+let quantityBag = document.querySelector("#qty-bag");
+bags = 0;
+quantityBag.textContent = bags;
+
+let quantityShoes = document.querySelector("#qty-shoes");
+shoes = 0;
+quantityShoes.textContent = shoes;
+
+
+let plus = document.querySelector(".btn--plus");
+let minus = document.querySelector(".btn--minus");
+
+plus.addEventListener("click", function() {
+    shoes++;
+    console.log(shoes);
+    quantityShoes.textContent = shoes;
+});
+
+minus.addEventListener("click", function() {
+    shoes--;
+    console.log(shoes);
+    quantityShoes.textContent = shoes;
+});
+
